114859df8c3a4fa84630c5419335763cc96f67a7
Oscar Lafarga <otech47@gmail.com>
2015-12-01 17:14:11 -0500

    kontakt, simplify commerce, sdk 23

--- app/src/main/java/co/setmusic/setwalletandroid/SetwalletMainActivity.java
+++ app/src/main/java/co/setmusic/setwalletandroid/SetwalletMainActivity.java
@@ -11,6 +11,7 @@ public class SetwalletMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setwallet_main);
+
     }
 
 }
---
+++ app/src/main/java/co/setmusic/setwalletandroid/adapters/CategoryListAdapter.java
@@ -0,0 +1,7 @@
+package co.setmusic.setwalletandroid.adapters;
+
+/**
+ * Created by oscarlafarga on 12/1/15.
+ */
+public class CategoryListAdapter {
+}
