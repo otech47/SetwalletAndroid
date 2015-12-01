4bd7e600a808958ce4a7d294eaa1d649f9f9efca
Oscar Lafarga <otech47@gmail.com>
2015-12-01 14:44:02 -0500

    Initial commit

---
+++ app/src/main/java/co/setmusic/setwalletandroid/SetwalletMainActivity.java
@@ -0,0 +1,16 @@
+package co.setmusic.setwalletandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SetwalletMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setwallet_main);
+    }
+
+}
